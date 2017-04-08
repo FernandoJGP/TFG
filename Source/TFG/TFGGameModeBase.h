4bc062f2371949534fac1d2a294ea5ef27cf4c8a
FernandoJGP <fernandogarcia1989@gmail.com>
2017-04-08 02:30:11 +0200

    Initial commit. Added project default files and gitignore.

---
+++ Source/TFG/TFGGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TFGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TFG_API ATFGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
