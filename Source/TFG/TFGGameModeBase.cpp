39d80464dd8eb95df4542a0bb9dede148f47d3c5
FernandoJGP <fernandogarcia1989@gmail.com>
2017-08-26 05:49:59 +0200

    Minor changes on game mode.

--- Source/TFG/TFGGameModeBase.cpp
+++ Source/TFG/TFGGameModeBase.cpp
@@ -8,4 +8,8 @@ ATFGGameModeBase::ATFGGameModeBase() : Super()
 	// Default pawn
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/MainCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// HUD
+	//static ConstructorHelpers::FClassFinder<UUserWidget> HUDWidgetClassFinder(TEXT("Class'/Game/Blueprints/HUDs/UserHUD/UserHUDWidget.UserHUDWidget_C'"));
+	//HUDClass = HUDWidgetClassFinder.Class;
 }
